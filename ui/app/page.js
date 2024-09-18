ac72ab240899f3b99a6f0d0fd2ef3662e2a30fc8
faisalill <mfaisal.ilyas.d@gmail.com>
2024-09-19 00:31:18 +0530

    Add Notifications ui

--- ui/app/page.js
+++ ui/app/page.js
@@ -1,15 +1,38 @@
 "use client"
 import Header from '@/sections/Header'
 import ImageInput from '@/sections/ImageInput'
-import { useState } from 'react';
-import Output from '@/sections/Output';
+import { useEffect, useState } from 'react';
+import { Toaster } from '@nyxui/toaster';
+import { useToast } from '@nyxui/use-toast';
 
 export default function Home() {
   const [backendUrl, setBackendUrl] = useState("http://localhost:5000");
+  const { toast } = useToast()
+
+  useEffect(() => {
+    if (process.env.NODE_ENV == 'development') {
+      setBackendUrl("http://localhost:5000")
+      toast({
+        title: "  Development Mode",
+        description: "Local Python Server at port 5000 will be used"
+      })
+    }
+    if (process.env.NODE_ENV == "production") {
+      setBackendUrl("https://payload-image-processing-d3eq.onrender.com")
+      toast({
+        title: "  Production Mode",
+        description: "Remote Python Server will be used."
+      })
+    }
+  }, [])
+
   return (
-    <div className='flex flex-col gap-8 mt-14 items-center dark mb-80'>
-      <Header />
-      <ImageInput backendUrl={backendUrl} />
-    </div>
+    <>
+      <div className='flex flex-col gap-8 mt-14 items-center dark mb-80'>
+        <Header />
+        <ImageInput backendUrl={backendUrl} />
+      </div>
+      <Toaster />
+    </>
   )
 }
