f8730c0da366f50634e4beb1cbe8722ba67a64f4
faisalill <mfaisal.ilyas.d@gmail.com>
2024-09-18 19:13:50 +0530

    Add Parameters

--- backend/algorithms/edge_detection.py
+++ backend/algorithms/edge_detection.py
@@ -1,6 +1,6 @@
 import cv2
  
-def edge_detection(img):
+def edge_detection(img, threshold1=100, threshold2=200):
 # Read the original image
 # Display original image
     
@@ -15,6 +15,6 @@ def edge_detection(img):
     # sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
 
 # Canny Edge Detection
-    edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) # Canny Edge Detection
+    edges = cv2.Canny(image=img_blur, threshold1=threshold1, threshold2=threshold2) # Canny Edge Detection
 # Display Canny Edge Detection Image 
     return edges
--- ui/app/sections/Output.jsx
+++ ui/app/sections/Output.jsx
@@ -4,18 +4,32 @@ import algorithm from "@/utils/algo_types";
 import { useState } from "react";
 import { Card, CardTitle } from "@nyxui/card";
 import { Image } from 'antd'
+import { Input } from "@nyxui/input";
+import { Label } from "@nyxui/label";
 
 export default function Output({ image, backendUrl }) {
   const [outputImageUrl, setOutputImageUrl] = useState();
+  const [edgeDetectionParams, setEdgeDetectionParams] = useState({
+    threshold1: 100,
+    threshold2: 200
+  });
+  const [erosionParams, setErosionParams] = useState({
+    kernel1: 5,
+    kernel2: 5
+  })
+  const [dilationParams, setDilationParams] = useState({
+    kernel1: 5,
+    kernel2: 5
+  })
 
-  const uploadImage = async (algoType) => {
+  const uploadImage = async (algoType, params) => {
     if (!image) return; // Exit if no image selected
 
     const formData = new FormData();
     formData.append('file', image);
 
     try {
-      await fetch(`${backendUrl}?algoType=${algoType}`, {
+      await fetch(`${backendUrl}?algoType=${algoType}${params}`, {
         method: 'POST',
         body: formData,
       })
@@ -51,11 +65,86 @@ export default function Output({ image, backendUrl }) {
           <TabsTrigger value="erosion">Erosion</TabsTrigger>
           <TabsTrigger value="dilation">Dilation</TabsTrigger>
         </TabsList>
-        <TabsContent value="edge_detection" className="tab_content">
-          <Button onClick={() => { uploadImage(algorithm.EdgeDetection) }}>Process Image</Button>
+        <TabsContent value="edge_detection" className="flex flex-col items-center mt-3 gap-3">
+          <div>
+            <Label>Upper Threshold</Label>
+            <Input placeholder="100" onChange={(e) => {
+              setEdgeDetectionParams({
+                ...edgeDetectionParams,
+                threshold1: e.target.value
+              })
+            }} />
+          </div>
+          <div>
+            <Label>Lower Threshold</Label>
+            <Input placeholder="200" onChange={(e) => {
+              setEdgeDetectionParams({
+                ...edgeDetectionParams,
+                threshold2: e.target.value
+              })
+            }} />
+          </div>
+          <Button onClick={() => {
+            const params = `&threshold1=${edgeDetectionParams.threshold1}&threshold2=${edgeDetectionParams.threshold2}`
+            uploadImage(algorithm.EdgeDetection, params)
+          }}>Process Image</Button>
         </TabsContent>
-        <TabsContent value="erosion">Erosion</TabsContent>
-        <TabsContent value="dilation">Dilation</TabsContent>
+        <TabsContent value="erosion" className="flex flex-col items-center gap-4">
+          <div>
+            <Label>Kernel Size</Label>
+            <div className="flex gap-4">
+              <Input placeholder="5"
+                onChange={(e) => {
+                  setErosionParams({
+                    ...erosionParams,
+                    kernel1: e.target.value
+                  })
+                }}
+              />
+              <Input placeholder="5"
+                onChange={(e) => {
+                  setErosionParams({
+                    ...erosionParams,
+                    kernel2: e.target.value
+                  })
+                }}
+              />
+            </div>
+            <p className="text-center mt-2 text-xl">{erosionParams.kernel1} x {erosionParams.kernel2}</p>
+          </div>
+          <Button onClick={() => {
+            const params = `&kernel1=${erosionParams.kernel1}&kernel2=${erosionParams.kernel2}`
+            uploadImage(algorithm.Erosion, params)
+          }}>Process Image</Button>
+        </TabsContent>
+        <TabsContent value="dilation" className="flex flex-col items-center">
+          <div>
+            <Label>Kernel Size</Label>
+            <div className="flex gap-4">
+              <Input placeholder="5"
+                onChange={(e) => {
+                  setDilationParams({
+                    ...dilationParams,
+                    kernel1: e.target.value
+                  })
+                }}
+              />
+              <Input placeholder="5"
+                onChange={(e) => {
+                  setDilationParams({
+                    ...dilationParams,
+                    kernel2: e.target.value
+                  })
+                }}
+              />
+            </div>
+            <p className="text-center mt-2 text-xl">{dilationParams.kernel1} x {dilationParams.kernel2}</p>
+          </div>
+          <Button onClick={() => {
+            const params = `&kernel1=${dilationParams.kernel1}&kernel2=${dilationParams.kernel2}`
+            uploadImage(algorithm.Dilation, params)
+          }}>Process Image</Button>
+        </TabsContent>
       </Tabs>
     </div>
   )
